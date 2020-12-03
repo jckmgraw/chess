67c13344fa08d4c7b43ab158cf2a55e31330f14e
jckmgraw <jckmcgraw@me.com>
2020-12-03 14:09:11 -0800

    removed redux framework template code

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { loggingMiddleware } from '../middleware/logging';
-import counterReducer from '../features/counter/counterSlice';
 import boardReducer from '../features/chess/board/boardSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     board: boardReducer,
   },
   middleware: [loggingMiddleware],
